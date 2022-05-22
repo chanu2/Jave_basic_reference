f7ee627bce762adbe894d55763e98f45011b9fc6
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-05-22 22:16:11 +0900

    Update data_type.java

--- grammar/data_type.java
+++ grammar/data_type.java
@@ -109,6 +109,90 @@
 
 
 
+//문자 타입
+// character set: 문자를 숫자로 변환한 값의 세트
+// encoding: 문자가 숫자로 변환되는 것
+// decoding: 숫자에서 다시 문자로 변환되는 것
+// ASKII code: 알파벳과 숫자 특수 문자등을 1바이트에 표현하는데 사용하는 문자세트
+// UNICODE: 전 세계 표준으로 만든 문자 세트
+// UTF-8: 1바이트에서 4바이트까지 다양하게 문자를 표현할 수 있음
+// UTF-16: 2바이트로 문자를 표현
 
 
+
+// char 2byte
+// package ch08;
+
+// public class CharacterTest {
+
+// 	public static void main(String[] args) {
+
+// 		char ch1 = 'A';
+// 		System.out.println(ch1);
+// 		System.out.println((int)ch1);  # 강제 정수로 전환
+		
+// 		char ch2 = 66;
+// 		System.out.println(ch2);     
+		
+// 		int ch3 = 67;
+// 		System.out.println(ch3);
+// 		System.out.println((char)ch3);
+		
+// 		//char ch4 = -66;  음수는 대입 할 수 없음
+
+//     		char ch5 = '한';
+// 		char ch6 = '\uD55C';
+		
+// 		System.out.println(ch5);
+// 		System.out.println(ch6);
+// 	}
+// }
+
+
+
+
+//자료형(data type)- 논리형과 자료형 없이 변수 사용하기
+
+
+
+// 논리형
+// true(참), false(거짓) 두 가지만 나타냄
+// 1바이트를 사용 함
+// 값이 존재하는지, 배열이 비었는지, 결과가 참인지 거짓인지등을 표현
+
+
+
+//지역 변수 자료형 없이 사용하기 (  자바 10 지원 됨 )
+
+
+// Local variable type inference
+// 추론 가능한 변수에 대한 자료형을 선언하지 않음
+// 한번 선언하여 추론 된 변수는 다른 타입의 값을 대입 할 수 없음
+// 지역 변수만 사용 가능
+
+
+
+// package ch09;
+
+// public class Global {
+
+// 	public static void main(String[] args) {
+// 		var i =10;
+// 		var j =10.0;
+// 		var str ="test";
+// 		System.out.println(i);
+// 		System.out.println(j);
+// 		System.out.println(str);
+		
+// 		var str2 =str;    
+// 		System.out.println(str2);
+		
+// 		str ="hellow";     // 다른 문자열을 할당할 수 있다.
+// 		//str=3;    //이미 문자열 데이터이기 떄문에 정수형 변수를 넣을 수 없다.
+		
+				
+		
+// 	}
+
+// }
 
