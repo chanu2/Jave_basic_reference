a522ade6fa94056cbc24634fecbe9aa56ea81e19
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-05-24 13:15:58 +0900

    Create  Operator.java

---
+++ grammar/Operator.java
@@ -0,0 +1,35 @@
+// 증감 연산자!!!!!
+
+
+/*
+public class OperaterTest {
+
+	public static void main(String[] args) {
+		int gameScore =150;
+		int lastScore = ++gameScore;   //gameScore+=1  // *statement가 끝나기전에 값이 증감된다.* 
+		System.out.println(lastScore);
+		System.out.println(gameScore);
+		
+		gameScore =159;
+		lastScore = gameScore++;     // statement가 끝나고 나서 값이 증가된다
+		
+		System.out.println(lastScore);
+		System.out.println(gameScore);
+		
+		
+		gameScore =170;
+		lastScore = --gameScore;     // statement가 끝나기 전에 값이 감소
+		
+		System.out.println(lastScore);
+		System.out.println(gameScore);
+		
+		
+		
+		gameScore =200;
+		lastScore = gameScore--;     // statement가 끝나고 나서 값이 감소
+		
+		System.out.println(lastScore);
+		System.out.println(gameScore);
+		
+		
+	} /*
