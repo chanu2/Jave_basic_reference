e77d05083f6f7b1a1dbe82aea9ab6dc4a80fe906
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-06-21 01:51:32 +0900

    Create List.java

---
+++ grammar/List.java
@@ -0,0 +1,48 @@
+package ch18;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];    //int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10}; 이렇게 쓸 수도 있다.
+		int total=0;
+		for(int i=0,num=1 ;i<arr.length;i++ ) {
+			arr[i]=num++;
+			
+		}
+			
+//		for (int i=0;i<arr.length;i++) {
+//			
+//			total+=arr[i];
+//		}
+		for(int num1 : arr) {                    // 처음부터 끝까지 돌때 사용된다 0~n-1
+	//이 엘리먼트가 해당되는 타입
+			total+=num1;    // num1에 순차적으로 값이 들어간다.
+		}
+			System.out.println(total);
+		
+		
+	}
+
+}
+
+
+//배열 초기화 하는법
+/*
+int[] arr1 = new int[10];  10개의 배열이 생성됨
+int arr2[] = new int[10];
+
+
+
+int[] numbers = new int[] {10, 20, 30};  //개수 생략해야 함
+
+int[] numbers = {10, 20, 30};            // new int[]  생략 가능 
+
+int[] ids; 
+ids = new int[] {10, 20, 30};            // 선언후 배열을 생성하는 경우는 new int[] 생략할 수 없음
+
+*/
+
+
+
+
+
