042470d81f638edda66eeaf222ec10ef73a9aded
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-06-21 22:52:09 +0900

    Create Subject.java

---
+++ Object_Oriented/ClassList/practice/Subject.java
@@ -0,0 +1,25 @@
+package ch24;
+
+public class Subject {
+	private String sbjectName;
+	private int subjectScore;
+	
+	public Subject() {   //안만들어도 댐
+		
+	}
+	public String getSbjectName() {
+		return sbjectName;
+	}
+	public void setSbjectName(String sbjectName) {
+		this.sbjectName = sbjectName;
+	}
+	public int getSubjectScore() {
+		return subjectScore;
+	}
+	public void setSubjectScore(int subjectScore) {
+		this.subjectScore = subjectScore;
+	}
+	
+	
+	
+}
