07932e99c19cf6af593355456cf1e8a859e957c8
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-03 23:23:43 +0900

    Create TreeDPrint1.java

---
+++ Object_Oriented/Data_structure/Generic_programming/Generic_unused/TreeDPrint1.java
@@ -0,0 +1,20 @@
+package ch06;
+
+public class TreeDPrint1 {
+	private Plastic material;
+	
+	
+	
+	public String toString() {
+		return "재료는 Plastic 입니다.";
+	}
+	
+	public Plastic getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Plastic material) {
+		this.material = material;
+	}
+	
+}
