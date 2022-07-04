cf9e37f0cd89d5efb92f7a8d608347f4571188de
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-04 23:40:46 +0900

    Create Meterial.java

---
+++ Object_Oriented/Data_structure/Generic_programming/T_extend_class/Meterial.java
@@ -0,0 +1,7 @@
+package ch07;
+
+public abstract class Meterial { // 이 클래스를 상속받아서 사용하고 단독적으로는 사용하지 않을 것 이기 때문에 absstract를 선언
+	public abstract void doPrinting();  // 재료들이 공통으로 사용할 매서드들을 선언해 준다
+		
+	
+}
