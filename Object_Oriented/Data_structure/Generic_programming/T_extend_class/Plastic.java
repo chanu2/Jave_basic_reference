0a119a8711b0e120d8dd4ee173c51e7103ba4fc9
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-04 23:41:03 +0900

    Create Plastic.java

---
+++ Object_Oriented/Data_structure/Generic_programming/T_extend_class/Plastic.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class Plastic extends Meterial {
+	public String toString() {        
+		return "재료는 Plastic입니다.";
+	}
+
+	@Override
+	public void doPrinting() {
+		
+	}
+
+}
