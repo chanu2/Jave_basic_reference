e1f626a6ca27a11f6281bca67608fa9690da66f2
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-11 23:41:17 +0900

    Create WhippingCream.java

---
+++ Object_Oriented/input,output_stream/Decorator_Pattern/WhippingCream.java
@@ -0,0 +1,14 @@
+package ch19;
+
+public class WhippingCream extends Decorator {
+	
+	public  WhippingCream(Coffee coffee) {  // 컨스트럭트를 넣어야 하는 이유는 상위클래스인 Decorator에서 디폴트 컨스트럭트가 없기 때문에
+		super(coffee);             // 하위클래스에서 반드시 매개변수가 있는 컨스트럭트가 슈퍼를 호출해야한다
+	}
+	
+	public void brewing() {
+		super.brewing(); // 슈퍼(상위클래스)의 제조법을 부르는 코드
+		System.out.println("Adding WhippingCream");
+	}
+	
+}
