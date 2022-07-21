bd2c375c5ac41a6b3f1796c751260d89c3c43f31
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-21 23:21:45 +0900

    Update ThreadTest .java

--- Object_Oriented/Thread/Thread_ Inheritance/ThreadTest .java
+++ Object_Oriented/Thread/Thread_ Inheritance/ThreadTest .java
@@ -1,11 +1,11 @@
 package ch20;
 
-class MyThread extends Thread{  // run을 구현해 줘야한다   
+class MyThread1 extends Thread{  // run을 구현해 줘야한다   // 이미 다른 것을 extends했을 때  class MyThread implements Runnable
 	
 	public void run() {   
 		
 		int i;
-		for(i = 0; i<200; i++) {   //200 전까지 더해주는 메서드
+		for(i = 0; i<200; i++) {
 			System.out.print(i + "\t");
 		}
 	}
@@ -16,8 +16,14 @@ public class ThreadTest {
 	public static void main(String[] args) {
 
 		System.out.println(Thread.currentThread()+"start");   // 현재 어떤 thread가 작동하는지 보여주는 코드
-		MyThread th1 = new MyThread();
-		MyThread th2 = new MyThread();
+		MyThread1 th1 = new MyThread1();
+		
+		//class MyThread implements Runnable 일때 
+		// MyThread runnable = new MyThread();  그냥 start 불가
+		//Thread th1 = new Thread(runnable);  이걸 쓰고 start 가능
+		//Thread th2 = new Thread(runnable);
+		
+		MyThread1 th2 = new MyThread1();
 		
 		th1.start();          // 총 3개의 thread가 실행된다  //
 		th2.start();
@@ -26,7 +32,7 @@ public class ThreadTest {
 		
 //		Runnable run = new Runnable() {
 //			
-//			@Override                       하나의 쓰레드가 돼서 바로 실행 가능하다(이런 방법도 있다)
+//			@Override                       하나의 쓰레드가 되서 바로 실행 가능하다(이런 방법도 있다)
 //			public void run() {
 //				System.out.println("run");
 //			}
