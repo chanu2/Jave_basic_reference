e809b7726cbe39d44e0a2d5f0dfee0784846f571
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-24 23:33:21 +0900

    Create BasicEvaluation.java

---
+++ Object_Oriented/final_project/Package_grade/BasicEvaluation.java
@@ -0,0 +1,33 @@
+package grade;
+
+public class BasicEvaluation implements  GradeEvaluation {
+
+	@Override
+	public String getGrade(int point) {
+		String grade;
+		
+		if(90 <= point) {
+			grade = "A";
+			
+		}
+		else if(80 <= point && point <= 89 ){
+			grade = "B";
+			
+		}
+		else if(70 <= point && point <= 79 ){
+			grade = "C";
+			
+		}
+		else if(55 <= point && point <= 69 ){
+			grade = "D";
+		}
+		else {
+			grade = "F";
+		}
+		
+		return grade;
+		
+		
+	}
+
+}
