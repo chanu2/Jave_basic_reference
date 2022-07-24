be950aa363f59aa0a364bd3022821dc34864b6fd
chanu2 <96942183+chanu2@users.noreply.github.com>
2022-07-24 23:50:12 +0900

    Create Define.java

---
+++ Object_Oriented/final_project/utils/Define.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Define {
+	
+	public static final int KOREAN = 1001;  //국어   // 정해진 국어 id값 
+	public static final int MATH = 2001;    //수학
+		
+	public static final int AB_TYPE = 0;    // A, B, C     // 변하지 않는 final로 선언한다
+	public static final int SAB_TYPE = 1;   // S, A, B, c
+	public static final int PF_TYPE = 2;   // P, F
+
+
+}
